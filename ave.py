459ebb074e0cc13605c6f8b8e5c062fece27ed1e
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-07-31 18:56:36 +0430

    Add files via upload

---
+++ ave.py
@@ -0,0 +1,19 @@
+import math
+
+name = input('whats your name?')
+lastname = input(' whats your lastname?')
+a = float (input('type the first lessons:'))
+b = float (input('type the second lessons:'))
+c = float (input('type the third lessons:'))
+
+x = (a + b + c ) / 3
+ print(x)
+
+ if x =<17 :
+     print( "great")
+
+ elif x= 17  and  x >= 12 :
+      print( "normal")   
+
+ elif x =< 12 :
+     print ("fail")     
