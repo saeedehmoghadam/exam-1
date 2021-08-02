e2606adbfde013ac47ea516e41c5d26a488aeb14
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-02 10:57:56 +0430

    Add files via upload

--- bmi.py
+++ bmi.py
@@ -2,16 +2,18 @@ import math
 h = float(input("type height ="))
 w = float(input("type weight ="))
 
+h= h/100
+
 bmi =(h ** 2) / w  
 
 if bmi < 18.5 :
-     print("weight dificiency" )
+ print("weight dificiency" )
 
-elif 18.5 > bmi < 24.5 : 
-    print ("normal")
+elif  18.5 > bmi < 24.5 : 
+ print ("normal")
 
-elif 25 > bmi < 29.9 :
-     print ("overwieght")
+elif 24.5 > bmi < 29.9 : 
+ print ("overwieght")
 
-elif bmi > 30 :
-     print("chubby")
+elif  bmi > 30 :
+ print ("fat")
