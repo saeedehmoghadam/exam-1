6ceb70f8cf08a5a5112aa90cd2bb845adbedf3b0
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-02 13:35:18 +0430

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,17 @@
+import math
+
+n = 0
+
+while  True :
+
+  s = input(" please enter your number : ")
+
+
+  if s == "exit" :
+     break
+    
+  else :
+     
+    n = int(s) + n 
+
+print (n)    
