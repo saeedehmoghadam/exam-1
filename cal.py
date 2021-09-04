494e7eab8c8e1b4509cfd7924fc266869f29090f
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-09-04 23:38:14 +0430

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,61 @@
+import math
+
+number_1 = int(input("Enter your the first number: "))
+number_2 = int(input("Enter your the second number: "))
+
+y = input ( '''
+ choose one of the under wiki :
++   addition
+-   subtraction
+*   multiplication
+/   division
+**  power
+fac  factorial
+sin  sinus
+cos  cosinus
+tan   tangant
+radi   sqrt 
+''' )
+
+if  y == '+' :
+    y_number = number_1 + number_2
+    print(number_1,"+", number_2,"=", y_number) 
+
+elif y == '-' :
+
+     y_number = number_1 - number_2
+     print(number_1,"-" ,number_2,"=", y_number)   
+     
+elif y == '*' :
+     y_number = number_1 * number_2
+     print(number_1,"*" ,number_2,"=",y_number)
+
+elif y == '/' :
+     y_number = number_1 / number_2
+     print(number_1,"/" ,number_2,"=", y_number)   
+
+elif  y == '**' :
+    y_number = number_1 ** number_2
+    print(number_1,"**", number_2,"=", y_number) 
+
+elif  y == 'fac' :
+    y_number = math.factorial(number_1)
+    print(number_1,"=", y_number) 
+
+elif  y == 'sin' :
+    y_number = math.sin(number_1)  
+    print(number_1,"=", y_number) 
+
+elif  y == 'cos' :
+    y_number = math.cos(number_1) 
+    print(number_1,"=", y_number) 
+
+elif  y == 'tan' :
+    y_number = math.tan(number_1) 
+    print(number_1,"=", y_number) 
+
+elif  y == 'radi' :
+    y_number = math.sqrt( number_1) 
+    print(number_1,"=", y_number) 
+else :
+  print ("run again")    
